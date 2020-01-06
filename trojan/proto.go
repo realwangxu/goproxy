e876f8df3e6e7d59aa8ca179f5861e6970fc7a8e
allen.w <koomoxs@gmail.com>
2020-01-06 20:29:17 +0800

    fix

--- trojan/proto.go
+++ trojan/proto.go
@@ -2,7 +2,10 @@ package trojan
 
 import (
 	"bytes"
+	"errors"
 	"github.com/koomox/goproxy/socks"
+	"io"
+	"net"
 )
 
 var (
@@ -149,6 +152,76 @@ func ParsePacket(b []byte) (addr, payload []byte) {
 	if bufferLen >= payloadLen {
 		payload = buffer[:payloadLen]
 		addr = address
+	}
+
+	return
+}
+
+func ParseUDPPacket(b []byte, c net.Conn) (addr, payload, buffered []byte, err error) {
+	var (
+		buf    []byte
+		buffer []byte
+	)
+	if b == nil {
+		buf = make([]byte, socks.UdpBufSize)
+		n, err := c.Read(buf)
+		if err != nil {
+			return
+		}
+		b = buf[:n]
+	}
+	buffer = b
+	addr = socks.SplitAddr(buffer)
+	if addr == nil {
+		err = errors.New("parse address failed!")
+		return
+	}
+
+	addrLen := len(addr)
+	buffer = b[addrLen:]
+	bLen := len(buffer)
+	if bLen < 4 {
+		buf = make([]byte, socks.UdpBufSize)
+		n, err := c.Read(buf[bLen:])
+		if err != nil {
+			return
+		}
+		copy(buf, buffer)
+		buffer = buf[:bLen+n]
+		bLen = len(buffer)
+		if bLen < 4 {
+			err = errors.New("udp packet short")
+			return
+		}
+	}
+
+	payloadLen := int(buffer[0])<<8 | int(buffer[1])
+	buffer = buffer[2:]
+	if !bytes.Equal(buffer[:2], CRLF) {
+		err = errors.New("parse udp packet not found CRLF")
+		return
+	}
+
+	buffer = buffer[2:]
+	bLen = len(buffer)
+	if bLen < payloadLen {
+		buf = make([]byte, payloadLen)
+		n, err := io.ReadFull(c, buf[bLen:])
+		if err != nil {
+			return
+		}
+		copy(buf, buffer)
+		buffer = buf[:bLen+n]
+		bLen = len(buffer)
+		if bLen < payloadLen {
+			err = errors.New("udp packet short")
+			return
+		}
+	}
+
+	payload = buffer[:payloadLen]
+	if bLen > payloadLen {
+		buffered = buffer[payloadLen:]
 	}
 
 	return
