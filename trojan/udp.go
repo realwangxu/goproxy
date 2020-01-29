55280f5f7b56ff24c2c78799611c26abdcc0d26b
allen.w <koomoxs@gmail.com>
2020-01-29 19:55:52 +0800

    fix

--- trojan/udp.go
+++ trojan/udp.go
@@ -2,90 +2,132 @@ package trojan
 
 import (
 	"bytes"
+	"errors"
 	"github.com/koomox/goproxy/socks"
+	"io"
 	"net"
 )
 
-type UDPPacket struct {
-	buf     []byte
-	addr    []byte
-	payload []byte
-	buffer  []byte
-}
-
-func NewUDPPacket() *UDPPacket {
-	return &UDPPacket{}
-}
-
-func (h *UDPPacket) Bytes() []byte {
-	return h.buf
-}
-
-func (h *UDPPacket) Addr() []byte {
-	return h.addr
-}
-
-func (h *UDPPacket) String() string {
-	if h.addr == nil {
-		return ""
+func MakeUDPacket(addr, payload []byte) []byte {
+	if addr == nil || payload == nil {
+		return nil
 	}
-	addr := socks.SplitAddr(h.addr)
-	if addr == nil {
-		return ""
-	}
-	_, host, port := addr.Parse()
-	return net.JoinHostPort(host, port)
-}
 
-func (h *UDPPacket) Payload() []byte {
-	return h.payload
-}
-
-func (h *UDPPacket) Buffer() []byte {
-	return h.buffer
-}
-
-func (h *UDPPacket) Generate(addr, payload []byte) {
-	var buf bytes.Buffer
-	h.addr = addr
-	h.payload = payload
-	buf.Write(addr)
+	addrLen := len(addr)
 	payloadLen := len(payload)
-	buf.WriteByte(byte(payloadLen >> 8))
-	buf.WriteByte(byte(payloadLen & 0xFF))
-	buf.Write(CRLF)
-	buf.Write(payload)
-	h.buf = buf.Bytes()
+	b := make([]byte, addrLen+2+2+payloadLen)
+	copy(b, addr)
+	offset := addrLen
+	b[offset] = byte(payloadLen >> 8)
+	b[offset+1] = byte(payloadLen)
+	offset += 2
+	copy(b[offset:], CRLF)
+	offset += 2
+	copy(b[offset:], payload)
+
+	return b
 }
 
-func (h *UDPPacket) Parse(buf []byte) bool {
-	h.buf = buf
-	address := socks.SplitAddr(buf)
+func ParseUDPacket(b []byte) (addr, payload []byte) {
+	address := socks.SplitAddr(b)
 	if address == nil {
-		return false
+		return
 	}
 
-	bufLen := len(buf)
-	offset := len(address)
-	h.addr = buf[:offset]
-	if bufLen <= offset+4 {
-		return false
+	addrLen := len(address)
+	n := len(b)
+	offset := addrLen
+	if n < offset+2+2 {
+		return
 	}
-	payloadLen := int(buf[offset])<<8 | int(buf[offset+1])
+
+	payloadLen := int(b[offset])<<8 | int(b[offset+1])
 	offset += 2
-	br := buf[offset:]
+	br := b[offset:]
 	if !bytes.Equal(br[:2], CRLF) {
-		return false
-	}
-	offset += 2
-	fullLen := offset + payloadLen
-	if bufLen < fullLen {
-		return false
-	}
-	h.payload = buf[offset:fullLen]
-	if bufLen > fullLen {
-		h.buffer = buf[fullLen:]
+		return
 	}
 
-	return true
+	offset += 2
+	buffer := b[offset:]
+	bufferLen := len(buffer)
+	if bufferLen >= payloadLen {
+		payload = buffer[:payloadLen]
+		addr = address
+	}
+
+	return
+}
+
+func ReadUDPacket(b []byte, c net.Conn) (addr, payload, buffered []byte, err error) {
+	var (
+		buf    []byte
+		buffer []byte
+		n      int
+	)
+	if b == nil {
+		buf = make([]byte, socks.UdpBufSize)
+		n, err = c.Read(buf)
+		if err != nil {
+			return
+		}
+		b = buf[:n]
+	}
+	buffer = b
+	addr = socks.SplitAddr(buffer)
+	if addr == nil {
+		err = errors.New("parse address failed!")
+		return
+	}
+
+	addrLen := len(addr)
+	buffer = b[addrLen:]
+	bLen := len(buffer)
+	if bLen < 4 {
+		buf = make([]byte, socks.UdpBufSize)
+		n, err = c.Read(buf[bLen:])
+		if err != nil {
+			return
+		}
+		copy(buf, buffer)
+		buffer = buf[:bLen+n]
+		bLen = len(buffer)
+		if bLen < 4 {
+			err = errors.New("udp packet short")
+			return
+		}
+	}
+
+	payloadLen := int(buffer[0])<<8 | int(buffer[1])
+	buffer = buffer[2:]
+	if !bytes.Equal(buffer[:2], CRLF) {
+		err = errors.New("parse udp packet not found CRLF")
+		return
+	}
+
+	buffer = buffer[2:]
+	bLen = len(buffer)
+	if bLen < payloadLen {
+		buf = make([]byte, payloadLen)
+		n, err = io.ReadFull(c, buf[bLen:])
+		if err != nil {
+			return
+		}
+		copy(buf, buffer)
+		buffer = buf[:bLen+n]
+		bLen = len(buffer)
+		if bLen < payloadLen {
+			err = errors.New("udp packet short")
+			return
+		}
+	}
+
+	payload = buffer[:payloadLen]
+	if bLen > payloadLen {
+		buf = buffer[payloadLen:]
+		buffered = make([]byte, len(buf))
+		copy(buffered, buf)
+	}
+
+	return
 }
