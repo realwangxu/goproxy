d99534ebf07675bc55c1c21812f907abd54c3714
allen.w <koomoxs@gmail.com>
2022-01-04 21:10:37 +0800

    fix

--- socks/ticker.go
+++ socks/ticker.go
@@ -4,7 +4,7 @@ import "time"
 
 func (r *Server) UpdateTicker(d time.Duration) {
 	r.handlerTicker()
-	time.AfterFunc(d, func() { r.UpdateTicker() }) // Minute
+	time.AfterFunc(d, func() { r.UpdateTicker(d) })
 }
 
 func (r *Server) handlerTicker() {
