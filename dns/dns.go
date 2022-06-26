877e1b48b0d094883e180057478608837aa68d37
allen.w <koomoxs@gmail.com>
2022-06-26 12:49:34 +0800

    commit

---
+++ dns/dns.go
@@ -0,0 +1,131 @@
+package dns
+
+import (
+	"errors"
+	"math/rand"
+	"net"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+type Server struct {
+	sync.RWMutex
+	servers    []string
+	timeout    time.Duration
+	cache      map[string]*Resolver
+	RetryTimes int
+	r          *rand.Rand
+}
+
+type Resolver struct {
+	ips  []net.IP
+	last time.Time
+}
+
+var current = &Server{}
+
+func Background() *Server {
+	return current
+}
+
+func New(servers []string, timeout time.Duration) *Server {
+	for i := range servers {
+		servers[i] = net.JoinHostPort(servers[i], "53")
+	}
+	server := &Server{servers: servers, timeout: timeout, cache: make(map[string]*Resolver), RetryTimes: len(servers) * 2, r: rand.New(rand.NewSource(time.Now().UnixNano()))}
+	return server
+}
+
+func WithBackground(server *Server, timeout time.Duration) {
+	current = server
+	current.dispatchLoop(timeout)
+}
+
+func (r *Server) LookupHost(host string) (result []net.IP, err error) {
+	if result = r.Get(host); result != nil {
+		return
+	}
+	if result, err = r.lookupHost(host, r.RetryTimes); err != nil {
+		return
+	}
+	r.Set(host, result)
+	return
+}
+
+func (r *Server) lookupHost(host string, triesLeft int) ([]net.IP, error) {
+	m1 := new(dns.Msg)
+	m1.Id = dns.Id()
+	m1.RecursionDesired = true
+	m1.Question = make([]dns.Question, 1)
+	m1.Question[0] = dns.Question{dns.Fqdn(host), dns.TypeA, dns.ClassINET}
+	in, err := dns.Exchange(m1, r.servers[r.r.Intn(len(r.servers))])
+
+	result := []net.IP{}
+
+	if err != nil {
+		if strings.HasSuffix(err.Error(), "i/o timeout") && triesLeft > 0 {
+			triesLeft--
+			return r.lookupHost(host, triesLeft)
+		}
+		return result, err
+	}
+
+	if in != nil && in.Rcode != dns.RcodeSuccess {
+		return result, errors.New(dns.RcodeToString[in.Rcode])
+	}
+
+	for _, record := range in.Answer {
+		if t, ok := record.(*dns.A); ok {
+			result = append(result, t.A)
+		}
+	}
+	return result, err
+}
+
+func (r *Server) Get(host string) []net.IP {
+	r.RLock()
+	defer r.RUnlock()
+
+	if d, ok := r.cache[host]; ok {
+		if time.Now().Sub(d.last) < r.timeout {
+			return d.ips
+		}
+	}
+
+	return nil
+}
+
+func (r *Server) Set(host string, ips []net.IP) {
+	if ips == nil || len(ips) == 0 {
+		return
+	}
+
+	r.Lock()
+	r.cache[host] = &Resolver{ips: ips, last: time.Now()}
+	r.Unlock()
+}
+
+func (r *Server) Remove(host string) {
+	r.Lock()
+	defer r.Unlock()
+
+	if _, ok := r.cache[host]; ok {
+		delete(r.cache, host)
+	}
+}
+
+func (r *Server) cleaner() {
+	if r != nil {
+		r.Lock()
+		r.cache = make(map[string]*Resolver, 1024)
+		r.Unlock()
+	}
+}
+
+func (r *Server) dispatchLoop(timeout time.Duration) {
+	r.cleaner()
+	time.AfterFunc(timeout, func() { r.dispatchLoop(timeout) })
+}
--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/koomox/goproxy
 
-go 1.17
+go 1.18
 
 require (
 	github.com/koomox/redblacktree v0.0.0-20210330113247-f46882cd075c
-	github.com/miekg/dns v1.1.45
-	github.com/oschwald/geoip2-golang v1.5.0
+	github.com/miekg/dns v1.1.50
+	github.com/oschwald/geoip2-golang v1.7.0
 )
 
 require (
-	github.com/oschwald/maxminddb-golang v1.8.0 // indirect
+	github.com/oschwald/maxminddb-golang v1.9.0 // indirect
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220325203850-36772127a21f // indirect
 	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
